49c9eda0458fb51a8317131734a9ede9c9d3ac13
alvaromsp87 <164578202+alvaromsp87@users.noreply.github.com>
2025-04-07 19:31:08 -0300

    Update route.ts

--- app/api/usuarios/[id]/route.ts
+++ app/api/usuarios/[id]/route.ts
@@ -1,9 +1,12 @@
 import { NextResponse } from "next/server";
 import { pool } from "@monitoriadigital/lib/db";
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+export async function DELETE(
+  request: Request,
+  context: { params: { id: string } }
+) {
   try {
-    const id = params.id;
+    const id = context.params.id;
     await pool.query("DELETE FROM usuarios WHERE id = ?", [id]);
     return NextResponse.json({ message: "Usuário excluído com sucesso" });
   } catch (e) {
